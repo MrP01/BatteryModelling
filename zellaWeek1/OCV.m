22c51eb6e8019a4a93a3fca18e12bf36d6bed6a1
MrP01 <peter@waldert.at>
2023-02-05 19:09:54 +0000

    pre-commit run --all-files

--- zellaWeek1/OCV.m
+++ zellaWeek1/OCV.m
@@ -1,7 +1,7 @@
 function openCurrentVoltage = OCV(stateOfChargeValue)
     % fits a 5th order polynomial to the discharging plot to get the OCV
     % as a function of SOC
-    dischargeData = importdata('dc2.mat'); 
+    dischargeData = importdata('dc2.mat');
     capacity = 2.9; %Ah
     x = flip((abs(min(dischargeData.Ah)) + dischargeData.Ah) /capacity);
     x = x(end - length(nonzeros(x)) +1:end);
