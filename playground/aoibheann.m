e011f24775f64b3e895f6ab15605a1b38586ddb9
Aoibheann1 <123953237+Aoibheann1@users.noreply.github.com>
2023-02-12 18:36:45 +0000

    mass matrix code

--- playground/aoibheann.m
+++ playground/aoibheann.m
@@ -1,68 +1,92 @@
-ocv = @(z) 10.8 * z.^5 - 26.06 * z.^4 + 22.74 * z.^3 - 8.4 * z.^2 + 1.6 * z + 3.47;
-iR1 = @(t) 2.899 * square(2 * pi * t / (60 * 60)) * pulse(t - 5 * 60) * pulse(20 * 60 - t);
-iR1(6 * 60)
-%%
+% A constant, singular mass matrix
+M = [1 0 0
+   0 1 0
+   0 0 0];
 
-figure(1)
-fplot(iR1, [0, 3600]);
-title('Input Current vs time');
+% Use the LSODI example tolerances.  The 'MassSingular' property is
+% left at its default 'maybe' to test the automatic detection of a DAE.
+options = odeset('Mass',M,'RelTol',1e-4,'AbsTol',[1e-6 1e-10 1e-6]);
+
+% Initialisation y(1)=z, y(2)=IR1(t), y(3)=v(t)
+y0_1 = [1,1,4.11];
+tspan_1 = linspace(0,2);
+[t,y] = ode15s(@f,tspan_1,y0_1,options);
+
+%y0 = y1(end,:)
+%tspan = linspace(0.1,0.2)
+
+
+
+%[t2,y2] = ode15s(@f,tspan,y0,options);
+%t = [t1;t2];
+%y = [y1;y2];
+
+
+
+
+
+
+
+figure(1);
+plot(t,y(:,1), 'bo');
+ylabel('z');
+title('state of charge');
 xlabel('t');
-ylabel('iR1(t)')
-%%
 
-%R1=4197*10^(-6); C1=1099; R0=3701*10^(-6); z0=1; Q=2.9/60;
-R1 = 15.8 * 10^(-3); C1 = 38 * 10^(3); R0 = 26 * 10^(-3); z0 = 1; Q = 2.9/3600; %Q=2.9/3600;
-
-%z = @(t) z0-1/Q*sin(t);
-z = @(t) z0 - 1 / Q * 2.899 * (t - 5 * 60) * pulse(t - 5 * 60) * pulse(20 * 60 - t);
-
-[t, itot] = ode45(@vdp1, [0 3600], 1);
-zeval = zeros(1, length(t));
-
-for i = 1:length(t)
-    zeval(i) = z(t(i));
-end
-
-figure(1)
-plot(t, itot);
-title('Total current vs time');
+figure(2);
+plot(t,y(:,2));
+ylabel('I(t)');
+title('current');
 xlabel('t');
-ylabel('i(t)')
-%%
-% Voltage
 
-v = zeros(1, length(t));
-
-for i = 1:length(t)
-    v(i) = ocv(z(t(i))) - R1 * iR1(t(i)) - R0 * itot(i);
-end
-
-figure(2)
-plot(t, v);
-title('Total voltage vs time');
-xlabel('t');
+figure(3);
+plot(t,y(:,3));
 ylabel('v(t)');
-%%
-figure(3)
-plot(t, zeval, 'g');
-title('SOC vs time')
-
+title('voltage');
 xlabel('t');
-ylabel('z(t)')
-%%
-function didt = vdp1(t, itot)
-    R1 = 15.8 * 10^(-3); C1 = 38 * 10^(3); R0 = 26 * 10^(-3); z0 = 1; Q = 2.9/3600; %Q=2.9/3600;
+% --------------------------------------------------------------------------
 
-    iR1 = @(t) 2.899 * square(2 * pi * t / (60 * 60)) * pulse(t - 5 * 60) * pulse(20 * 60 - t);
-    didt = -1 / (R1 * C1) * iR1(t) + 1 / (R1 * C1) * itot;
+
+%iR2 = @(t) 1+square(t);
+%fplot(iR2)
+
+
+
+
+function out = f(t,y)
+out = [  -I(t).\Q(t)
+    -1\(R1(y(1)).*C1(y(1))).*y(2)+1\(R1(y(1)).*C1(y(1))).*I(t)
+   y(3) - ocv(y(1))+ y(2).*R1(y(1))+I(y(1)).*R0(y(1))  ];
 end
 
-function x = pulse(t)
 
-    if t >= 0
-        x = 1;
-    else
-        x = 0;
-    end
+function C = C1(z)
+    C = 38*10^(3)*z.^2;
+%    C = 38*10^(3);
+end
+function R = R1(z)
+    R = 15.8*10^(-3)*z.^2;
+%    R = 15.8*10^(-3);
 
+end
+function R = R0(z)
+    R = 26*10^(-3)*z.^2;
+%    R = 26*10^(-3);
+
+end
+function q = Q(t)
+    q = 2.9/60-10^(-6)*t;
+%    q = 2.9/60;
+
+end
+
+
+function ir = I(t)
+    ir = 1;
+end
+
+
+
+function o = ocv(z)
+    o = 10.8*z.^5-26.06*z.^4+22.74*z.^3-8.4*z.^2+1.6*z+3.47;
 end
